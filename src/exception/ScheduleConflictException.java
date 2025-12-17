115a6f17ee2cfa8a38b37474bde2bb63e67abc55
chikanora <chikanorta@gmail.com>
2025-12-17 10:14:16 -0700

    Created classes

---
+++ src/exception/ScheduleConflictException.java
@@ -0,0 +1,7 @@
+package exception;
+
+public class ScheduleConflictException extends Exception {
+    public ScheduleConflictException(String message) {
+        super(message);
+    }
+}
---
+++ src/exception/StaffShortageException.java
@@ -0,0 +1,7 @@
+package exception;
+
+public class StaffShortageException extends Exception {
+    public StaffShortageException(String message) {
+        super(message);
+    }
+}
